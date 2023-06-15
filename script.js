bd7aed162c5c235264485da25afa7323461148e3
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-15 22:00:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 //your JS code here. If required.
-function addText() {
-	document.body.innerText = "DOM load success";
-}
-window.onload(addText());
+document.addEventListener('DOMContentLoaded', function() {
+  // Create a new <p> element
+  var paragraph = document.createElement('p');
+
+  // Set the text content of the paragraph
+  paragraph.textContent = 'DOM load success';
+
+  // Clear the existing content of the <body> element
+  document.body.innerHTML = '';
+
+  // Append the paragraph to the <body> element
+  document.body.appendChild(paragraph);
+});
